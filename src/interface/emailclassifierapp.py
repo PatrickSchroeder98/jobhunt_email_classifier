2de3b0a15ae1507718b9540556fe8a0d7c4957ab
Patrick Schröder <patrickschroder479@gmail.com>
2025-10-21 17:49:05 +0200

    Added method for 3 stage pipelines training
    Added method responsible for training of 3-stage-pipeline, updated init to create constant with classifiers.

--- src/interface/emailclassifierapp.py
+++ src/interface/emailclassifierapp.py
@@ -1,6 +1,6 @@
 from src.classifiers.classifier import Classifier
 from src.data_module.dataloader import DataLoader
-from src.exceptions.exceptions import PathError
+from src.exceptions.exceptions import PathError, ClassifierOptionError, ModelNotFound
 from src.models.model import Model
 
 
@@ -14,8 +14,10 @@ class EmailClassifierApp:
         self.model1 = None
         self.model2 = None
         self.model3 = None
-        self.CLASSIFIERS = ["MultinomialNB", "LogisticRegression"]
-        self.CLASSIFIER_METHODS = [self.classifier.set_clf_nb, self.classifier.set_clf_lr]
+        self.CLASSIFIERS = {
+            "MultinomialNB": self.classifier.set_clf_nb,
+            "LogisticRegression": self.classifier.set_clf_lr,
+        }
 
     def set_model1_clf(self, clf):
         """Method sets model object with provided classifier and returns it."""
@@ -45,4 +47,48 @@ class EmailClassifierApp:
 
     def classifier_option_check(self, option):
         """Method that checks if provided classifier option is valid."""
-        return False if option not in self.CLASSIFIERS else True
+        return False if option not in self.CLASSIFIERS.keys() else True
+
+    def train_3_stage_pipelines(
+        self,
+        path1="../data/training_emails_stage_1.csv",
+        path2="../data/training_emails_stage_2.csv",
+        path3="../data/training_emails_stage_3.csv",
+        classifier_option_1="MultinomialNB",
+        classifier_option_2="MultinomialNB",
+        classifier_option_3="MultinomialNB",
+        column_name_1="related_to_jobhunt",
+        column_name_2="is_confirmation",
+        column_name_3="is_invitation",
+        column_name_main="email_text",
+    ):
+        """Method trains 3 pipelines with the usage of data given in argument. Default option is the data from 'data' folder"""
+
+        def train_pipeline(path, classifier_option, setup, column_name_train):
+            """Helper function for training pipeline."""
+            try:
+                df = self.load_data_csv(path)
+                if not self.classifier_option_check(classifier_option):
+                    raise ClassifierOptionError
+                else:
+                    # uses the key of dictionary and calls the corresponding method
+                    self.CLASSIFIERS[classifier_option]()
+            except ClassifierOptionError as e:
+                print(e.get_message())
+                print("Error code: " + e.get_code())
+                self.model1 = None
+                self.model2 = None
+                self.model3 = None
+                return None
+            else:
+                model = setup(self.classifier.get_classifier())
+                model.build_pipeline()
+                model.set_X(df[column_name_main])
+                model.set_y(df[column_name_train])
+                model.train()
+
+
+        train_pipeline(path1, classifier_option_1, self.set_model1_clf, column_name_1)
+        train_pipeline(path2, classifier_option_2, self.set_model2_clf, column_name_2)
+        train_pipeline(path3, classifier_option_3, self.set_model3_clf, column_name_3)
+
