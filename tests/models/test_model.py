930df2124a03e9054ee621e4a9a25beb2ec6b5ce
Patrick Schröder <patrickschroder479@gmail.com>
2025-11-01 16:51:33 +0100

    Second update to Model tests
    Added more tests for the Model class.

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -1,6 +1,8 @@
 import unittest
 from src.models.model import Model
 from src.classifiers.classifier import Classifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class TestModel(unittest.TestCase):
 
@@ -118,6 +120,100 @@ class TestModel(unittest.TestCase):
 
         model.y_test = [0, 1, 2]
         self.assertEqual(model.get_y_test(), [0, 1, 2])
+
+        del clf, model
+
+    def test_set_X(self):
+        """Method tests the set_X method of the model."""
+        clf = Classifier()
+        model = Model(clf)
+
+        model.set_X([0, 1, 2])
+        self.assertEqual(model.X, [0, 1, 2])
+
+        del clf, model
+
+    def test_get_X(self):
+        """Method tests the get_X method of the model."""
+        clf = Classifier()
+        model = Model(clf)
+
+        model.X = [0, 1, 2]
+        self.assertEqual(model.get_X(), [0, 1, 2])
+
+        del clf, model
+
+    def test_set_y(self):
+        """Method tests the set_y method of the model."""
+        clf = Classifier()
+        model = Model(clf)
+
+        model.set_y([0, 1, 2])
+        self.assertEqual(model.y, [0, 1, 2])
+
+        del clf, model
+
+    def test_get_y(self):
+        """Method tests the get_y method of the model."""
+        clf = Classifier()
+        model = Model(clf)
+
+        model.y = [0, 1, 2]
+        self.assertEqual(model.get_y(), [0, 1, 2])
+
+        del clf, model
+
+    def test_set_test_size(self):
+        """Method tests the set_test_size method of the model."""
+        clf = Classifier()
+        model = Model(clf)
+
+        model.set_test_size(0.3)
+        self.assertEqual(model.test_size, 0.3)
+
+        del clf, model
+
+    def test_get_test_size(self):
+        """Method tests the get_test_size method of the model."""
+        clf = Classifier()
+        model = Model(clf)
+
+        model.test_size = 0.3
+        self.assertEqual(model.get_test_size(), 0.3)
+
+        del clf, model
+
+    def test_get_pipeline(self):
+        """Method tests the get_pipeline method of the model."""
+        clf = Classifier()
+        clf.set_clf_nb()
+        model = Model(clf)
+
+        p = Pipeline([
+            ("tfidf", TfidfVectorizer()),
+            ("classifier", model.clf)
+        ])
+        model.pipeline = p
+
+        self.assertEqual(model.get_pipeline(), p)
+
+    def test_set_random_state(self):
+        """Method tests the set_random_state method of the model."""
+        clf = Classifier()
+        model = Model(clf)
+
+        model.set_random_state(42)
+        self.assertEqual(model.random_state, 42)
+
+        del clf, model
+
+    def test_get_random_state(self):
+        """Method tests the get_random_state method of the model."""
+        clf = Classifier()
+        model = Model(clf)
+
+        model.random_state = 42
+        self.assertEqual(model.get_random_state(), 42)
 
         del clf, model
 
