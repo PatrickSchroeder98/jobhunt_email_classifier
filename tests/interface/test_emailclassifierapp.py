3ef3919e970266bded7eb8ef6ff0fda7492f32c6
Patrick Schröder <patrickschroder479@gmail.com>
2025-11-28 19:25:40 +0100

    Update to interface tests
    Finished first test case, added second test case.

--- tests/interface/test_emailclassifierapp.py
+++ tests/interface/test_emailclassifierapp.py
@@ -2,6 +2,7 @@ import unittest
 from src.classifiers.classifier import Classifier
 from src.data_module.dataloader import DataLoader
 from src.interface.emailclassifierapp import EmailClassifierApp
+from src.models.model import Model
 
 
 class TestEmailClassifierApp(unittest.TestCase):
@@ -41,6 +42,39 @@ class TestEmailClassifierApp(unittest.TestCase):
             expected_val = CLASSIFIERS[key]
             actual_val = app.CLASSIFIERS[key]
             self.assertIsInstance(actual_val, type(expected_val))
+
+        self.assertEqual(
+            self.assertIsInstance(data_loader, DataLoader),
+            self.assertIsInstance(app.data_loader, DataLoader),
+        )
+
+        self.assertEqual(
+            self.assertIsNone(model1),
+            self.assertIsNone(app.model1),
+        )
+
+        self.assertEqual(
+            self.assertIsNone(model2),
+            self.assertIsNone(app.model2),
+        )
+
+        self.assertEqual(
+            self.assertIsNone(model3),
+            self.assertIsNone(app.model3),
+        )
+
+        self.assertEqual(
+            self.assertIsNone(multiclassifier_model),
+            self.assertIsNone(app.multiclassifier_model),
+        )
+
+    def test_set_model1_clf(self):
+        """Method tests the set_clf_vtc method of the class."""
+        app = EmailClassifierApp()
+        output = app.set_model1_clf("ExampleClf")
+        self.assertEqual(app.model1.clf, "ExampleClf")
+        self.assertIsInstance(output, Model)
+
 
 if __name__ == "__main__":
     unittest.main()
