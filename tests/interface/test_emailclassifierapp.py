050b167f0d3b625ef075c39eb4f81dde5e8c1e4b
Patrick Schröder <patrickschroder479@gmail.com>
2026-01-24 17:49:21 +0100

    39th update to interface tests
    Added test regarding the set_stacking_classifier_parameters method exception route when estimator options are incorrect.

--- tests/interface/test_emailclassifierapp.py
+++ tests/interface/test_emailclassifierapp.py
@@ -1499,7 +1499,6 @@ class TestEmailClassifierApp(unittest.TestCase):
             "MultinomialNB": MagicMock()
         }
 
-        # One estimator invalid
         app.classifier_option_check = MagicMock(
             side_effect=[True, False, True]
         )
@@ -1508,6 +1507,34 @@ class TestEmailClassifierApp(unittest.TestCase):
             estimator_1="InvalidEstimator",
             estimator_2="InvalidEstimator",
             estimator_3="MultinomialNB",
+        )
+
+        self.assertIsNone(result)
+
+        # No classifier building should happen
+        app.classifier.set_sc_clf_1.assert_not_called()
+        app.classifier.set_sc_clf_2.assert_not_called()
+        app.classifier.set_sc_clf_3.assert_not_called()
+        app.classifier.set_clf_stc.assert_not_called()
+
+    def test_set_stacking_classifier_estimators_invalid_estimator_1_3(self):
+        """Method that tests set_stacking_classifier_estimators exception route when estimator_1 and estimator_3 is invalid."""
+        app = EmailClassifierApp()
+        app.classifier = MagicMock()
+
+        # Registry with only one valid option
+        app.classifier.ESTIMATORS_AND_CLASSIFIERS = {
+            "MultinomialNB": MagicMock()
+        }
+
+        app.classifier_option_check = MagicMock(
+            side_effect=[True, False, True]
+        )
+
+        result = app.set_stacking_classifier_estimators(
+            estimator_1="InvalidEstimator",
+            estimator_2="MultinomialNB",
+            estimator_3="InvalidEstimator",
         )
 
         self.assertIsNone(result)
